a7f9e8c3bc7bbcadbeda19ec7059f915bce68d48
Delsune <167645505+Delsune@users.noreply.github.com>
2026-09-13 18:37:58 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import base64
 import secrets
 import tkinter as tk
 import datetime as dt
-import cryptography as crypt
 from cryptography.fernet import Fernet
+from cryptography import fernet
+from cryptography import exceptions
 
-key = 0
+
+key = None
 BYTES = 32
+
+
+def message(text):
+    msg = tk.Label(root, text=text)
+    msg.pack()
+    msg.after(3000, lambda: msg.destroy())
 
 
 def get_key():
@@ -21,72 +29,68 @@ def get_key():
         root.destroy()
 
     else:
-        message("The inputted key must be exactly 32 characters!", 125, 90)
+        message("The inputted key must be exactly 32 characters!")
 
 
 def generate():
-    secure_pass = secrets.token_bytes(BYTES)
+    secure_pass = secrets.token_urlsafe(BYTES)
     time_of_creation = dt.datetime.now()
 
     try:
         with open(r"Password.txt", "a", encoding="utf-8") as f:
             f.write("\n" + str(secure_pass) + " at " + str(time_of_creation) + "\n")
 
-        message("Password Generated: " + str(secure_pass), 2.5, 30)
+        message("Password Generated: " + str(secure_pass))
     except PermissionError:
-        message("Couldn't access file, permission denied", 2.5, 30)
+        message("Couldn't access file, permission denied")
 
 
 def encrypt_file():
     try:
-        fernet = Fernet(key)
+        fernet_key = Fernet(key)
 
         with open(r"Password.txt", "rb") as f:
             normal = f.read()
 
-        encrypted = fernet.encrypt(normal)
+        encrypted = fernet_key.encrypt(normal)
 
         with open(r"Password.txt", "wb") as f:
             f.write(encrypted)
 
-        message("File Encrypted", 2.5, 100)
-    except NameError:
-        message("There is no key for encryption!", 2.5, 100)
+        message("File Encrypted")
+
+    except TypeError:
+        message("There is no key for encryption!")
     except FileNotFoundError:
-        message("The file does not exist!", 2.5, 100)
+        message("The file does not exist!")
     except PermissionError:
-        message("Couldn't access file, permission denied", 2.5, 100)
-
-
-def message(text, x, y):
-    msg = tk.Label(root, text=text)
-    msg.place(x=x, y=y)
-    msg.after(3000, lambda: msg.destroy())
+        message("Couldn't access file, permission denied")
 
 
 def decrypt_file():
     try:
-        fernet = Fernet(key)
+        fernet_key = Fernet(key)
 
         with open(r"Password.txt", "rb") as f:
             encrypted = f.read()
 
-        decrypted = fernet.decrypt(encrypted)
+        decrypted = fernet_key.decrypt(encrypted)
 
         with open(r"Password.txt", "wb") as f:
             f.write(decrypted)
 
-        message("File Decrypted", 2.5, 100)
-    except NameError:
-        message("There is no key for decryption!", 2.5, 100)
-    except crypt.fernet.InvalidToken:
-        message("Cannot decrypt an unencrypted text file!", 2.5, 100)
-    except crypt.exceptions.InvalidSignature:
-        message("The key is not the same!", 2.5, 100)
+        message("File Decrypted")
+
+    except TypeError:
+        message("There is no key for decryption!")
+    except fernet.InvalidToken:
+        message("Cannot decrypt an unencrypted text file!")
+    except exceptions.InvalidSignature:
+        message("The key is not the same!")
     except FileNotFoundError:
-        message("The file does not exist!", 2.5, 100)
+        message("The file does not exist!")
     except PermissionError:
-        message("Couldn't access file, permission denied", 2.5, 100)
+        message("Couldn't access file, permission denied")
 
 
 root = tk.Tk()
@@ -111,8 +115,8 @@ make_pass = tk.Button(root, text='Generate Password', command=lambda: generate()
 file_encrypt = tk.Button(root, text='Encrypt File', command=lambda: encrypt_file())
 file_decrypt = tk.Button(root, text='Decrypt File', command=lambda: decrypt_file())
 
-make_pass.place(x=5, y=5)
-file_encrypt.place(x=5, y=75)
-file_decrypt.place(x=120, y=75)
+make_pass.pack()
+file_encrypt.pack()
+file_decrypt.pack()
 
 root.mainloop()
